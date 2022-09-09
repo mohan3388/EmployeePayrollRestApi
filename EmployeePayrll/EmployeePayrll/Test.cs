5f4ae07440d897b9fbe98d963a82528ff6638460
mohan <sahumohan338@gmail.com>
2022-09-09 18:36:16 +0530

    [mohan]add: Update Employee Data

---
+++ EmployeePayrll/EmployeePayrll/Test.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrll
+{
+    [TestClass]
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public long salary { get; set; }
+
+        RestClient client;
+        [TestInitialize]
+        public void Setup()
+        {
+            client = new RestClient("http://localhost:4000");
+        }
+
+        private IRestResponse getEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.GET);
+            IRestResponse restResponse = client.Execute(request);
+            return restResponse;
+        }
+        [TestMethod]
+        public void onCallingGetApi_returnEmployees()
+        {
+            IRestResponse restResponse = getEmployee();
+            Assert.AreEqual(restResponse.StatusCode, System.Net.HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(restResponse.Content);
+            Assert.AreEqual(2, dataResponse.Count());
+            foreach (Employee e in dataResponse)
+            {
+                Console.WriteLine("Id: " + e.id + ", name: " + e.name + ", salary: " + e.salary);
+            }
+        }
+        [TestMethod]
+        public void AddEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jobject = new JObject();
+            jobject.Add("Id", 1);
+            jobject.Add("name", "Kajal");
+            jobject.Add("salary", 5000);
+
+            request.AddParameter("application/json", jobject, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual(1, dataResponse.id);
+            Assert.AreEqual("Kajal", dataResponse.name);
+            Assert.AreEqual(5000, dataResponse.salary);
+            System.Console.WriteLine(response.Content);
+        }
+        [TestMethod]
+        public void onCallingPutApi_returnEmployees()
+        {
+            RestRequest request = new RestRequest("/employees/2", Method.PUT);
+            JObject jobject = new JObject();
+            jobject.Add("Id", 1);
+            jobject.Add("name", "Kajal");
+            jobject.Add("salary", 5000);
+            request.AddParameter("application/json", jobject, ParameterType.RequestBody);
+            var response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual(1, dataResponse.id);
+            Assert.AreEqual("Kajal", dataResponse.name);
+            Assert.AreEqual(5000, dataResponse.salary);
+            System.Console.WriteLine(response.Content);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
